26d0eef0b5378d2f83b987e1b8a1d01efe10f018
Abhijit1018 <abhijeetrathore104@gmail.com>
2025-02-27 08:43:30 +0530

    Create ADD TWO NUMBER.py

---
+++ ADD TWO NUMBER.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_dict = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+
+# Example usage:
+solution = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+print(solution.twoSum(nums, target))  # Output: [0, 1]
